1542a8b34b7afe2beffb96db7c87ca4a6b975c73
Kosmas Papadatos <kosmas.papadatos@gmail.com>
2015-04-25 17:03:09 +0300

    Test

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 var http = require('http');
 
 module.exports.start = function () {
-
+    /*
     http.createServer(function (req, res) {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end('Hello World1\n');
     }).listen(80, '104.155.37.54');
-
+    */
     http.createServer(function (req, res) {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end('Hello World2\n');
