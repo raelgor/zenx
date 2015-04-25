26846c7a5ac318371674da9f08435629fd16fba1
Kosmas Papadatos <kosmas.papadatos@gmail.com>
2015-04-25 17:13:37 +0300

    Test

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 var http = require('http');
 
 module.exports.start = function () {
+
     /*
     http.createServer(function (req, res) {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end('Hello World1\n');
     }).listen(80, '104.155.37.54');
     */
+
     http.createServer(function (req, res) {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end('Hello World2\n');
-    }).listen(80, '104.155.23.6');
+    }).listen(80, '10.240.212.94');//'104.155.23.6');
 
     console.log("ZenX Server Started.");
 
